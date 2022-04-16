89926bb06ef7aefb6149cffc580772bd951f39ff
JeniferMorati <ariel.betti@gmail.com>
2022-04-16 19:37:37 -0300

    feat(home): create first pokemon list render

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,20 +1,49 @@
-import { FC, useEffect, useState} from "react";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+} from "@mui/material";
+import { FC, useEffect } from "react";
 import { useRecoilState, useRecoilValueLoadable } from "recoil";
 import { atomPokemonList } from "../state/atoms";
 import { getAllSeasonOnePokemons } from "../state/selectors";
 
 const Home: FC<any> = () => {
-  const [pokemonDefaultList, setPokemonDefaultList] = useRecoilState(atomPokemonList);
-  const loadablePokemonList = useRecoilValueLoadable(getAllSeasonOnePokemons('?limit=151'));
+  const [pokemonDefaultList, setPokemonDefaultList] =
+    useRecoilState(atomPokemonList);
+  const loadablePokemonList = useRecoilValueLoadable(
+    getAllSeasonOnePokemons("?limit=151")
+  );
 
   useEffect(() => {
-    setPokemonDefaultList(loadablePokemonList.contents);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    setPokemonDefaultList(loadablePokemonList.contents?.results);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loadablePokemonList.state]);
 
-  console.log(pokemonDefaultList);
+  if (loadablePokemonList.state === "loading") {
+    return <div>carregando...</div>;
+  }
 
-  return <div>Home</div>;
+  if (loadablePokemonList.state === "hasError") {
+    return <div>error</div>;
+  }
+
+  return (
+    <List>
+      {pokemonDefaultList?.map((item: any, index: number) => (
+        <ListItem key={item.name}>
+          <ListItemAvatar>
+            <Avatar
+              src={`https://cdn.traction.one/pokedex/pokemon/${index + 1}.png`}
+            />
+          </ListItemAvatar>
+          <ListItemText primary={item.name} />
+        </ListItem>
+      ))}
+    </List>
+  );
 };
 
 export default Home;
